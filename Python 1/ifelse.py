11f1c77eeb8370384f13b0ace94417e4f8d44601
Ju5ty13 <ju5ty13welch@gmail.com>
2024-10-22 15:15:46 -0400

    First commit :D

---
+++ Python 1/ifelse.py
@@ -0,0 +1,12 @@
+coolness = 50
+
+if coolness < 50:
+    print("umm no aura points??")
+elif coolness > 50:
+    print("woah you're pretty cool, you got aura")
+else:
+    print("umm what bludslawg yappalappin about")
+
+# Separate check for coolness equal to 50
+if coolness == 50:
+    print("mid aura")
---
+++ Python 1/variables.py
@@ -0,0 +1,8 @@
+name = "meowsicles"
+age = "30"
+COOL_LEVEL = 10000
+
+print(name)
+print (age)
+print(COOL_LEVEL)
+print("meow meow meow meow meowww :D")
