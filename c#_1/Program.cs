9d5701bb5ed54e33c6f5ef176c08fda14a6b9041
Ju5ty13 <Ju5ty13Welch@gmail.com>
2025-01-09 17:42:36 -0500

    updated folders and made some code :D

---
+++ c#_1/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("this my first project");
+Console.WriteLine("hello");
+Console.WriteLine("meow meow meow meow meow meow meow meow meow meow meow meow meow meow meow meow meow meow meow meow meow meow meow meow meow meow emow meow meow meow meow meow meow meow");
